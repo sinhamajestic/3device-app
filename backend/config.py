fe031f9a63faab15443667fa264639a746211b91
Aditya Sinha <115427994+sinhamajestic@users.noreply.github.com>
2025-09-15 00:29:18 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,10 @@
-from pydantic import BaseSettings
-
+from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
     AUTH0_DOMAIN: str
     AUTH0_API_AUDIENCE: str
     MAX_SESSIONS: int = 3
-
     class Config:
         env_file = ".env"
 
 settings = Settings()
-
-
